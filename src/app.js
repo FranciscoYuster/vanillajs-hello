2f17a55b8361cb05bcafc029b0c10d749b4a6d1a
Francisco Yuster <yusterfrancisco@gmail.com>
2025-01-24 17:02:03 +0000

    Form

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function(){
+  const dialog = document.querySelector('#error-msg');
+  const form = document.querySelector('#sendForm');
+  const fields = document.querySelectorAll('input,textarea,select');
+  dialog.classList.add("d-none");
+  form.addEventListener("click", function(e){    
+    dialog.classList.remove("d-none");
+    fields.forEach(function(field){
+      field.style.backgroundColor = "#f8d7da";
+    });
+  });
+
+}
